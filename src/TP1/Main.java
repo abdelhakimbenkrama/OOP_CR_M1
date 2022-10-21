fca5f14a3b4bc85d580392c0c35ca8b1727f0eff
abdelhakimbenkrama <abdalhakim.3.benkrama1@gmail.com>
2022-10-22 00:19:28 +0100

    refactoring

---
+++ src/TP1/Main.java
@@ -0,0 +1,23 @@
+package TP1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // print statement
+        System.out.println("Hello world!");
+        //Data Types, Variables, and Arrays
+        int a = 1 ;
+        float x = 1.7f;
+        double c = 1.09;
+        Boolean k = true ;
+        String name = "Hakim"; // use "" for String and '' for char
+        char j = 'j';
+
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(k);
+        System.out.println(name);
+        System.out.println(j);
+    }
+}
---
+++ src/TP2/Main.java
@@ -0,0 +1,9 @@
+package TP2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // print statement
+        System.out.println("Tp ");
+    }
+}
