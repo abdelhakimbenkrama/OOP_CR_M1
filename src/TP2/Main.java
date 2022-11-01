05899428bcdee17b62830173302464a02b2a7812
abdelhakimbenkrama <abdalhakim.3.benkrama1@gmail.com>
2022-11-01 21:52:58 +0100

    tp2 + tp3

--- src/TP2/Main.java
+++ src/TP2/Main.java
@@ -2,8 +2,46 @@ package TP2;
 
 public class Main {
     public static void main(String[] args) {
+        int [] days_count , Years ;
+        // arrays
+        days_count = new int[31];
+        days_count[0]= 1;
+        days_count[1]= 2;
+        days_count[2]= 3;
+        days_count[3]= 4;
+        //System.out.println(days_count[3]);
+        // fill array once
+        String [] Months = {
+                "Jan" , "Fev" , "Mars" , "Avr"
+        };
+        //System.out.println(Months[3]);
 
-        // print statement
-        System.out.println("Tp ");
+
+        // fill Array with for loop
+        Years = new int[12];
+        for (int i =0 ; i < 12; i++){
+            Years[i] = 2000 + i;
+           // System.out.println(Years[i]);
+        }
+        // methods with local vars
+        int x = Adding(1,2);
+        //System.out.println(x);
+
+        // 2d array of days
+
+        int [][] daysIn2d;
+        daysIn2d = new int[5][5];
+        for( int i = 0; i<5 ; i++){
+            for( int j =0 ; j<5 ; j++){
+                daysIn2d[i][j] = (i+1)*(j+1);
+                System.out.println(daysIn2d[i][j]);
+            }
+        }
+
+
+    }
+
+    public static int Adding( int a , int b){
+        return 99;
     }
 }
---
+++ src/TP3/Main.java
@@ -0,0 +1,47 @@
+package TP3;
+class Box {
+    double w ,h ,d;
+    // Constructor
+
+    void volume(){
+        System.out.println("Volume = "+w*h*d);
+    }
+}
+
+class Box2 {
+    double w ,high ,depth;
+    // Constructor
+    Box2(double w , double h ,double d){
+        this.w =w;
+        high =h;
+        depth = d;
+    }
+    double volume(){
+        return w * high * depth;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        double v ;
+
+        Box b1 =  new Box();
+        Box2 b2 =  new Box2( 3.6 , 3.6 ,4.3);
+
+        b1.w =9.7 ;
+        b1.h =6.7 ;
+        b1.d =2.7 ;
+
+
+        // box volume
+        //v= b1.w*b1.h*b1.d;
+        //System.out.println(v);
+        v = b2.volume();
+        System.out.println(v);
+
+
+
+    }
+}
